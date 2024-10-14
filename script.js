9f90698cca37d4994a6df727cccd72b247ec9aee
theMungai <mungaithesimon@gmail.com>
2024-10-14 22:52:04 +0300

    MDN - DOM basics

---
+++ script.js
@@ -0,0 +1,16 @@
+const link = document.querySelector("a");
+link.textContent = "Mozilla Developer Network";
+link.href = "https://developer.mozilla.org";
+
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope that you enjoyed the ride.";
+
+sect.appendChild(para);
+
+const text = document.createTextNode(
+    " - the premier source for web development knowledge."
+);
+
+const linkpara = document.querySelector("p");
+linkpara.appendChild(text);
